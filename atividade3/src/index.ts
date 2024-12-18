0cfd0700a4395aa63c0111da2da96f40d85f18c0
Lucas Henrique <lhenrique5786@gmail.com>
2024-12-18 02:28:12 -0300

    atualização do codigo versão final
    Versão final do projeto

--- atividade3/src/index.ts
+++ atividade3/src/index.ts
@@ -6,6 +6,24 @@ const numbers = [64, 25, 12, 22, 11];
 const sortedNumbers = selectionSort(numbers, (a, b) => a - b);
 console.log("Ordenado (números):", sortedNumbers); 
 
-const strings = ["banana", "apple", "grape", "orange"];
+const strings = ["banana", "carro", "uva", "boi"];
 const sortedStrings = selectionSort(strings, (a, b) => a.localeCompare(b)); 
 console.log("Ordenado (strings):", sortedStrings);
+
+
+type People = {
+    name: string;
+    age: number;
+};
+
+const peopleList: People[] = [
+    { name: "Bruno", age: 25 },
+    { name: "Lucas", age: 20 },
+    { name: "Larissa", age: 30 }
+];
+
+const sortedPeople = selectionSort(peopleList, (a,b)=> a.age - b.age);
+console.log(sortedPeople);
+
+const sortedPeopleByName = selectionSort(peopleList,(a,b)=>a.name.localeCompare(b.name));
+console.log(sortedPeopleByName);
